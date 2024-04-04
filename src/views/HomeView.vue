2d0ca8305befa8890016bdd0813ef7832b1d180f
魏航宇 <weihangyu@bytedance.com>
2024-04-04 18:21:03 +0800

    feat: 精简代码

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <a-layout>
+    <a-layout-header style="background: #fff; padding: 0"></a-layout-header>
     <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <div class="logo" />
       <a-menu
         v-model:selectedKeys="state.selectedKeys"
         v-model:openKeys="state.openKeys"
@@ -12,21 +12,6 @@
       >
       </a-menu>
     </a-layout-sider>
-    <!-- <a-layout>
-      <a-layout-header style="background: #fff; padding: 0">
-        <menu-unfold-outlined
-          v-if="collapsed"
-          class="trigger"
-          @click="() => (collapsed = !collapsed)"
-        />
-        <menu-fold-outlined v-else class="trigger" @click="() => (collapsed = !collapsed)" />
-      </a-layout-header>
-      <a-layout-content
-        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
-      >
-        Content
-      </a-layout-content>
-    </a-layout> -->
     <router-view></router-view>
   </a-layout>
 </template>
@@ -35,15 +20,19 @@ import { h, reactive, ref } from 'vue'
 import { MailOutlined, MenuUnfoldOutlined, MenuFoldOutlined } from '@ant-design/icons-vue'
 import { useRouter } from 'vue-router'
 const router = useRouter()
-// const selectedKeys = ref<string[]>(['1'])
 const collapsed = ref<boolean>(false)
 const state = reactive({
   collapsed: false,
-  selectedKeys: ['1'],
+  selectedKeys: ['2'],
   openKeys: ['sub1'],
   preOpenKeys: ['sub1']
 })
 const items = reactive([
+  {
+    key: 'sub1',
+    label: '系统首页',
+    title: '系统首页'
+  },
   {
     key: 'sub1',
     icon: () => h(MailOutlined),
@@ -51,19 +40,9 @@ const items = reactive([
     title: '智能体管理',
     children: [
       {
-        key: '1',
-        label: '故事集列表',
-        title: '故事集列表'
-      },
-      {
         key: '2',
         label: '智能体列表',
         title: '智能体列表'
-      },
-      {
-        key: '3',
-        label: '故事集详情',
-        title: '故事集详情'
       }
     ]
   }
